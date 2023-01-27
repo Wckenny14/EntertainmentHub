c9a304c72ddc301740ce41a2c56f5a495be04999
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2023-01-27 11:35:46 +1100

    nearly there

--- app/controllers/rockpaperscissor_controller.rb
+++ app/controllers/rockpaperscissor_controller.rb
@@ -3,9 +3,39 @@ class RockpaperscissorController < ApplicationController
   end
 
   def answer
-    guess =['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
-    @throw = params[:throw].capitalize
-    @computer_guess = guess[rand(5)]
+    @throw = params[:throw]
+    @computer_guess = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'].sample
+    # if @throw != guess
+    #     return "logic dictates that your answer is illogical. Please try again"
+    # else
+      if @throw == 'Rock' && @computer_guess == 'Scissors'
+          @result = "#{@throw} smashes #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Scissors' && @computer_guess == 'Paper'
+          @result = "#{@throw} cuts #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Paper' && @computer_guess == 'Rock'
+          @result = "#{@throw} covers #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Rock' && @computer_guess == 'Lizard'
+          @result = "#{@throw} crushes #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Lizard' && @computer_guess == 'Spock'
+          @result = "#{@throw} poisons #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Spock' && @computer_guess == 'Scissors'
+          @result = "#{@throw} smashes #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Scissors' && @computer_guess == 'Lizard'
+          @result = "#{@throw} decapitates #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Lizard' && @computer_guess == 'Paper'
+          @result = "#{@throw} eats #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Paper' && @computer_guess == 'Spock'
+          @result = "#{@throw} disproves #{@computer_guess}. YOU WIN!"
+      elsif @throw == 'Spock' && @computer_guess == 'Rock'
+          @result = "#{@throw} vaporizes #{@computer_guess}. YOU WIN!"
+      elsif @throw == @computer_guess
+          @result = "The computer also chose #{@computer_guess} so you draw!"
+      else
+          @result = "#{@computer_guess} beats #{@throw}. YOU LOSE!"
+      end
+    # end
+end
+end
 #     @win_conditions = {
 #         'Rock' => 'Scissors',
 #         'Scissors' => 'Paper',
@@ -27,31 +57,3 @@ class RockpaperscissorController < ApplicationController
 #     end
 #   end
 # end
-
-      if @throw == 'Rock' && @computer_guess == 'Scissors'
-          @result = "#{@throw} smashes #{@computer_guess}. YOU WIN!"
-      elsif @throw == 'Scissors' && @computer_guess == 'Paper'
-          @result = "#{@throw} cuts #{@computer_guess}. YOU WIN!"
-      elsif @throw == 'Paper' && @computer_guess == 'Rock'
-          @result = "#{@throw} wraps #{@computer_guess}. YOU WIN!"
-      elsif @throw == 'Rock' && @computer_guess == 'Lizard'
-          @result = "#{@throw} smashes #{@computer_guess}. YOU WIN!"
-      elsif @throw == 'Lizard' && @computer_guess == 'Spock'
-          @result = "#{@throw} poisons #{@computer_guess}. YOU WIN!"
-      elsif @throw == 'Spock' && @computer_guess == 'Scissors'
-          @result = "#{@throw} smashes #{@computer_guess}. YOU WIN!"
-      elsif @throw == 'Scissors' && @computer_guess == 'Lizard'
-          @result = "#{@throw} cuts #{@computer_guess} in half. YOU WIN!"
-      elsif @throw == 'Lizard' && @computer_guess == 'Paper'
-          @result = "#{@throw} eats #{@computer_guess}. YOU WIN!"
-      elsif @throw == 'Paper' && @computer_guess == 'Spock'
-          @result = "#{@throw} disproves #{@computer_guess}. YOU WIN!"
-      elsif @throw == 'Spock' && @computer_guess == 'Rock'
-          @result = "#{@throw} smashes #{@computer_guess}. YOU WIN!"
-      elsif @throw == @computer_guess
-          @result = "The computer also chose #{@computer_guess} so you draw"
-      else
-          @result = "#{@computer_guess} beats #{@throw} so you lose"
-      end
-    end
-end
